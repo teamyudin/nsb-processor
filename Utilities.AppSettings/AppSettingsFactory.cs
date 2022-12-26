dfdc20b2924676e2ec0669a618b1f2701a23e0de
Voidray <team.yudin@gmail.com>
2022-12-25 20:06:28 -0600

    fix docker files

---
+++ Utilities.AppSettings/AppSettingsFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Utilities.AppSettings
+{
+    public static class AppSettingsFactory
+    {
+        public static T GetSettings<T>()
+        {
+            var consfiguration = GetConfiguration();
+            var result = consfiguration.GetSection(typeof(T).Name).Get<T>();
+            return result;
+        }
+
+        private static IConfigurationRoot GetConfiguration()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var builder = new ConfigurationBuilder()
+                  .AddJsonFile($"appsettings.json", true, true)
+                  .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                  .AddEnvironmentVariables();
+            var configuration = builder.Build();
+
+            return configuration;
+        }
+    }
+}
